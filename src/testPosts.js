2693716cdba091d4aec67b04390655cefe7cf4f7
Alex <alexzhang1931@gmail.com>
2018-05-06 16:59:36 +1000

    Add extra fields to the first element for PostDetail page

--- src/testPosts.js
+++ src/testPosts.js
@@ -8,7 +8,12 @@ const testPosts = [
         rating: 3,
         price: 100,
         review: 18,
-        guestNum: 2
+        guestNum: 2,
+        description: "On recommend tolerably my belonging or am. Mutual has cannot beauty indeed now sussex merely you. It possible no husbands jennings ye offended packages pleasant he. Remainder recommend engrossed who eat she defective applauded departure joy. Get dissimilar not introduced day her apartments. Fully as taste he mr do smile abode every. Luckily offered article led lasting country minutes nor old. Happen people things oh is oppose up parish effect. Law handsome old outweigh humoured far appetite. Remain valley who mrs uneasy remove wooded him you.Her questions favourite him concealed.We to wife face took he.The taste begin early old why since dried can first.Prepared as or humoured formerly.Evil mrs true get post.Express village evening prudent my as ye hundred forming.Thoughts she why not directly reserved packages you.Winter an silent favour of am tended mutual. Behind sooner dining so window excu",
+        provide: ["line1", "line2", "line3"],
+        required: ["line1", "line2", "line3"],
+        accessibility: "On recommend tolerably my belonging or am. Mutual has cannot beauty indeed",
+        address: "123 ABC street, DEF"
     },
     {
         title: "Card Title - 28 Characters",
