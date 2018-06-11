57525352f9a8d7b8a91fc602519f8c9e8300fa98
Alex <alexzhang1931@gmail.com>
2018-06-11 13:10:59 +1000

    Connect to backend via apollo client [unfinished]

--- src/reducers/post.js
+++ src/reducers/post.js
@@ -1,6 +1,24 @@
 import testPosts from '../testPosts';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+
+
+function simplequery() {
+  const client = new ApolloClient({
+    uri: 'http://45.32.189.215:3000/graphql'
+  });
+
+  client.query({
+    query: gql`
+    { userByName(user: \"Kai\") { name } }
+  `,
+  })
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+}
 
 const post = (state = testPosts[0], action) => {
+  simplequery();
   switch(action.type) {
     case 'EDIT_DESCRIPTION':
       let newState = Object.assign({}, state);
