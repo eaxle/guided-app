a15e2b43a8bbfabfcfc1a193e47e480c9504e4ec
ramesh khadka <eaxle07@gmail.com>
2018-07-28 18:55:03 +1000

    latest changes

--- src/screens/login/NameRegistrationScreen.js
+++ src/screens/login/NameRegistrationScreen.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 import MetaTags from 'react-meta-tags';
 import { NavLink } from 'react-router-dom';
+import validator from 'validator';
 import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
 class NameRegistrationScreen extends Component {
   constructor(props) {
     super(props);
-    this.state={ email:'', fName:'',lName:''}
+    this.state={ email:'', fName:'',lName:'',disable:true}
     this.state.email=this.props.location.state.email;
     this.handleFormData = this.handleFormData.bind(this);
+    this.toogleButton= this.toogleButton.bind(this);
+
   }
 
   handleFormData(event) {
-    console.log(event.target);
-    if(event.target.name=="fName"){
+   // console.log(event.target);
+    if(event.target.name==="fName"){
         this.setState({fName: event.target.value});
     }else{
     this.setState({lName: event.target.value});
     }
-    console.log(JSON.stringify(this.state))
+   if ((!this.state.fName.toString().trim().length) || (!this.state.lName.toString().trim().length) ) {
+          this.setState({disable:true})
+         }else{
+          this.setState({disable:false})
+         }
   }
+
+
+    toogleButton(event){
+    //console.log("sd");
+        if ((!this.state.fName.toString().trim().length) || (!this.state.lName.toString().trim().length) ) {
+       event.preventDefault();
+      }
+      }
 
   render() {
     return (
@@ -36,14 +52,14 @@ class NameRegistrationScreen extends Component {
 <Form>
     <div className="row">
         <div className="col-sm-12">
-            <input type="text" placeholder="First Name" name="fName" value={this.state.fName} onChange={this.handleFormData} className="col-sm-4 form-control form-control-sm  " />
+            <Input type="text" placeholder="First Name" name="fName" value={this.state.fName} onChange={this.handleFormData} className="col-sm-4 form-control form-control-sm  " />
             </div>
             <div className="col-sm-12">
-            <input type="text" placeholder="Last Name" name="lName" value={this.state.lName} onChange={this.handleFormData} className="col-sm-4 form-control form-control-sm  " />
+            <Input type="text" placeholder="Last Name" name="lName" value={this.state.lName} onChange={this.handleFormData} className="col-sm-4 form-control form-control-sm  " />
             </div>
-            <NavLink to={{pathname:"/BirthdayRegistrationScreen",
+            <NavLink onClick={this.toogleButton} to={{pathname:"/BirthdayRegistrationScreen",
                                                state:{value:this.state}}}
-                          className="btn  btn-success">
+                          className="btn  btn-success" disabled={this.state.disable}>
                         Continue
                        </NavLink>
             </div>
