97432635fbb458a0285a7267b6022c9151bdd72c
Lisharabbit <lisaqiu763@gmail.com>
2018-07-31 10:12:51 +1000

    3rd page in registration

--- src/screens/login/NameRegistrationScreen.js
+++ src/screens/login/NameRegistrationScreen.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import MetaTags from 'react-meta-tags';
 import {Provider} from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import './loginStyles.css';
+
+// import {
+//     AppRegistry,
+//     StyleSheet,
+//     Text,
+//     View,
+//     Dimensions,
+// } from 'react-native';
+//
+// const {screenWidth,screenHeight} = Dimensions.get('window')
+
 class NameRegistrationScreen extends React.Component {
   constructor(props) {
   console.log(props.value)
@@ -32,30 +44,41 @@ class NameRegistrationScreen extends React.Component {
            <MetaTags>
                <meta name="viewport" content="width=device-width, initial-scale=1" />
            </MetaTags>
-           <div className="row col-sm-12 text-center font-weight-bold text-capitalize h2">welcome to the guided
+
+           <div className="row col-sm-12 welcome">Welcome to Guided
            </div>
            <div className="row">
-
-           <div className="col-sm-12 font-weight-bold h4">
-               Choose an account creation option</div>
+           <div className="col-sm-12 font-weight-bold subtitle">
+               <p>Whats your name? </p>
+           </div>
+           <div className="col-sm-12 subsubtitle">
+             <p>Please enter the name you use in real life</p>
+           </div>
            </div>
 
+
     <div className="row">
-        <div className="col-sm-12">
+      <div className="inputs">
+            <div className="col-sm-12 inputName">
             <input type="text" placeholder="First Name" value={this.state.fName} onChange={this.handleFname} className="col-sm-4 form-control form-control-sm  " />
             </div>
-            <div className="col-sm-12">
+            <div className="col-sm-12 inputName">
             <input type="text" placeholder="Last Name" value={this.state.lName} onChange={this.handleLname} className="col-sm-4 form-control form-control-sm  " />
             </div>
+        </div>
             <NavLink
-                         to="/BirthdayRegistrationScreen" className="btn  btn-success">
+                         to="/BirthdayRegistrationScreen" className="btn  btncreate generalbtn">
                         Continue
                        </NavLink>
             </div>
+              <div className="row font-weight-bold h5">Already have an account?</div>
+
           <div className="row">
 
          </div>
+         <footer>step 1 of 6</footer>
                      </div>
+
     );
   }
 }
