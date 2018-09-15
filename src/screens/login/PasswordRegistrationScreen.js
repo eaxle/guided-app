fd5446798bf9b9a121fb1368397c8aaca8996537
ramesh khadka <eaxle07@gmail.com>
2018-09-15 13:30:05 +1000

    latest chnages

--- src/screens/login/PasswordRegistrationScreen.js
+++ src/screens/login/PasswordRegistrationScreen.js
@@ -127,16 +127,10 @@ class PasswordRegistrationScreen extends Component {
         }).then(res => {
             localStorage.clear();
             ;
-            /*this.title="SUCCESS!"
-            this.body="You've been successfully registered in the GUIDED. Please login to continue!"
-            document.getElementsByClassName("modal").show();*/
             alert('success')
 
         }).catch(err => {
             alert('error')
-            /*this.title="ERROR!"
-            this.body="registration unsuccessful!. Please try again!";
-            document.getElementsByClassName("modal").show();*/
 
         })
     }
@@ -200,24 +194,7 @@ class PasswordRegistrationScreen extends Component {
 
                     <footer className="page-footer footer-costomized">step 5 of 6</footer>
                 </div>
-                {/*<div className="modal" tabIndex="-1" role="dialog">
-                    <div className="modal-dialog" role="document">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">{this.title}</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                <p>{this.body}</p>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>*/}
+
             </div>
 
         );
