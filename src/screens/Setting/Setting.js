991b122ffd18d45426696f1eb3786e4fa6f17544
ramesh khadka <eaxle07@gmail.com>
2018-11-03 11:47:46 +1100

    latest changes

--- src/screens/Setting/Setting.js
+++ src/screens/Setting/Setting.js
@@ -26,31 +26,36 @@ class Setting extends Component {
 
                         </ListGroupItem>
                         <ListGroupItem className="subtext-left" hearder=""><NavLink
-                            to={{pathname: "/setting/officialdoc"}}>Official Documents</NavLink>
+                            to={{pathname: "/setting/officialdoc"}}><h4>Official Documents</h4></NavLink>
                             <h6>Upload government ID and other certifications.</h6></ListGroupItem>
 
-                        <ListGroupItem className="subtext-left"><NavLink to={{pathname: "/setting/payment"}}>Payments
-                            Settings
+                        <ListGroupItem className="subtext-left"><NavLink to={{pathname: "/setting/payment"}}>
+                            <h4>Payments Settings</h4>
                             <h6>Manage your transact.</h6></NavLink></ListGroupItem>
 
-                        <ListGroupItem className="subtext-left"><NavLink to={{pathname: "/setting/security"}}>Security
+                        <ListGroupItem className="subtext-left"><NavLink to={{pathname: "/setting/security"}}>
+                            <h4>Security</h4>
                             <h6>Change your password.</h6></NavLink></ListGroupItem>
 
-                        <ListGroupItem className="subtext-left"><NavLink to={{pathname: "/setting/privacy"}}>Privacy
+                        <ListGroupItem className="subtext-left"><NavLink to={{pathname: "/setting/privacy"}}>
+                            <h4>Privacy</h4>
                             <h6>Control your information.</h6></NavLink></ListGroupItem>
 
-                        <ListGroupItem className="subtext-left"><NavLink to={{pathname: "/setting/ad"}}>Advertising
+                        <ListGroupItem className="subtext-left"><NavLink to={{pathname: "/setting/ad"}}>
+                            <h4>Advertising</h4>
                             <h6>Set your preferences and learn how our ads work.</h6></NavLink></ListGroupItem>
 
-                        <ListGroupItem className="subtext-left"><NavLink to={{pathname: "/setting/notification"}}>Notifications
+                        <ListGroupItem className="subtext-left"><NavLink to={{pathname: "/setting/notification"}}>
+                            <h4>Notifications</h4>
                             <h6>Decide how we should contact you.</h6></NavLink></ListGroupItem>
 
-                        <ListGroupItem className="subtext-left"><NavLink to={{pathname: "/setting/media"}}>Media
+                        <ListGroupItem className="subtext-left"><NavLink to={{pathname: "/setting/media"}}>
+                            <h4>Media</h4>
                             <h6>Manage image, video, and sound controls.</h6></NavLink></ListGroupItem>
 
-                        <ListGroupItem className="subtext-left">Your Guided Activity
+                        <ListGroupItem className="subtext-left"><h4>Your Guided Activity</h4>
                             <h6>View, download, and delete your information.</h6></ListGroupItem>
-                        <ListGroupItem className="subtext-left">Legals and Policies
+                        <ListGroupItem className="subtext-left"><h4>Legals and Policies</h4>
                             <h6>Our expectations and commitments to users.</h6></ListGroupItem>
                     </ListGroup>
                 </div>
